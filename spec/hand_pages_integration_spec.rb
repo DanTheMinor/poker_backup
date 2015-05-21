91be3bf1d0c72b31aa21f35f62a7603e0a35a683
Jesse James <jrjames.pdx@gmail.com>
2015-05-21 15:39:06 -0700

    write integration spec for bb folding at flop

--- spec/hand_pages_integration_spec.rb
+++ spec/hand_pages_integration_spec.rb
@@ -221,3 +221,22 @@ describe("two player, sb fold", type: :feature) do
     expect(game.current_hand.winner_id).to be > 0
   end
 end
+
+describe("two player, bb fold", type: :feature) do
+  it("test big blind fold at flop") do
+    game = Game.create(name: 'showdown')
+    game.players.create(name: 'ben', stack: 200)
+    game.players.create(name: 'peter', stack: 200)
+    visit "/game/#{game.id}"
+    click_link "Deal first hand"
+    #start pre-flop
+    click_button "call"
+    click_button "check"
+    fill_in "amount", :with => "2"
+    click_button "Bet/Raise"
+    fill_in "amount", :with => "2"
+    click_button "Bet/Raise"
+    click_button "fold"
+    expect(game.current_hand.winner_id).to be > 0
+  end
+end
